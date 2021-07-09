9620a8ef893a8962b2513fbaa994a955b2704269
Ranveer Shah <ranveer2001s@gmail.com>
2021-07-09 20:28:12 +0530

    Added support for weather icons

--- src/Pages/MyLoc/Forecast.jsx
+++ src/Pages/MyLoc/Forecast.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import moment from 'moment';
 import { Card } from "react-bootstrap";
 import Day from "../assets/Day.png";
-
+import Night from "../assets/Night.png";
 const Forecast = ({weatherData}) => (
+      
     <div>
-          <h1>Location: {weatherData.name}</h1>
-          <p>Temprature: {weatherData.main.temp} &deg;C</p>
-          <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-          <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-          <p>Description: {weatherData.weather[0].main}</p>
-          <p>Humidity: {weatherData.main.humidity} %</p>
-          <p>Day: {moment().format('dddd')}</p>
-          <p>Date: {moment().format('LL')}</p>
-
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={Day} />
+          
+          <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src={Day}/>
             <Card.Body>
                   <Card.Title>Weather Station: {weatherData.name}</Card.Title>
                   <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                  <p>Day: {moment().format('dddd')}</p>
+                  <p>Date: {moment().format('LL')}</p>
+                  <p>Time {moment().format('HH:mm')} {console.log(Date.now())}</p>
+                        <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}/>
+                  <p>Temprature: {weatherData.main.temp} &deg;C</p>
+                  <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-GB',{hour: '2-digit', minute:'2-digit'})}</p>
+                  <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-GB',{hour: '2-digit', minute:'2-digit'})}</p>
+                  <p>Description: {weatherData.weather[0].main}</p>
+                  <p>Humidity: {weatherData.main.humidity} %</p>
+
                   </Card.Text>
             </Card.Body>
            </Card>
