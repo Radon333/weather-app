95c73dbf4da12af0d0a9b30abeb40e250d31eb3d
Ranveer Shah <ranveer2001s@gmail.com>
2021-07-10 13:52:53 +0530

    v0.1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home/Home"
+import Current from "./Pages/Current/Current"
+function App() {  
+  return (
+    <Switch>
+        <Route path="/current-weather">
+          <Current/>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+  );
+}
+
+export default App;
