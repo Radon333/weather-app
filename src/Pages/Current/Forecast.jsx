f870f5b072ce583a1428bbbf6ea15a3d7bd76160
Ranveer Shah <ranveer2001s@gmail.com>
2021-07-10 13:29:31 +0530

    Update Forecast.jsx

--- src/Pages/Current/Forecast.jsx
+++ src/Pages/Current/Forecast.jsx
@@ -1,29 +1,49 @@
 import React from 'react'
 import moment from 'moment';
-import Day from "../assets/Day.png";
-import Night from "../assets/Night.png";
 import "../Current/forecast.css"
+import {motion} from "framer-motion";
+import {Link} from "react-router-dom";
+import { AwesomeButton } from "react-awesome-button";
 const Forecast = ({weatherData}) => (
-      
-    <div id="myloc-main">
-          <div className="card">
-          <div id="carditems">
-                  <div className="cardimgcontainer">
-                  <img id="cardimg" src={(weatherData.sys.sunrise<=Date.now() && Date.now()<weatherData.sys.sunset)? Day : Night}/>
-                  </div>
-                  <h5>Estimated Location: {weatherData.name}</h5>
-                  <p>Date: {moment().format('LL')}</p>
-                  <p>Temprature: {weatherData.main.temp} &deg;C</p>
-                  <p id="des-container"> <h6 className="test">{weatherData.weather[0].main}</h6>
-                        <img alt="weather-icon" className="test" src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}/>
+<div id="bodybg">
+      <h2 id="cwdtitle">Current Weather Data</h2>
+<div className="main">
+      <div className="test">
+            <h5>Estimated Location: {weatherData.name}</h5>
+            
+            <p>Date: {moment().format('LL')}</p>
+            <p>Temprature: {weatherData.main.temp} &deg;C</p>
+                  <p id="des-container"> <h6 >{weatherData.weather[0].main}</h6>
+                        <img alt="weather-icon"  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}/>
                   </p>
+            
+      
+
+            
+                 
+                 <p>Humidity: {weatherData.main.humidity}% &nbsp; Wind Speed: {weatherData.wind.speed}m/s</p>
+            
+
+            
                   <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-GB',{hour: '2-digit', minute:'2-digit'})} &nbsp;
                   Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-GB',{hour: '2-digit', minute:'2-digit'})}</p>
-
-                  <p>Humidity: {weatherData.main.humidity}% &nbsp; Wind Speed: {weatherData.wind.speed}m/s</p>
+           
+                  
                   </div>
-    </div>
-    </div>
+                             
+</div>
+           <div id="fcbtn-container">
+           <Link to="/">
+            <AwesomeButton
+                type="secondary"
+                size="medium"
+                ripple
+            >
+            Home
+            </AwesomeButton>
+            </Link>
+            </div>
+</div>
   )
   
   export default Forecast;
