45576d3c7031b836bb94f85e3126a2ce068eb717
Ranveer Shah <ranveer2001s@gmail.com>
2021-07-17 22:28:48 +0530

    Added city selector basic logic

---
+++ src/Pages/Current/CitySelector.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {Row, Col, FormControl, Button} from 'react-bootstrap';
+
+const CitySelector = () => {
+  const [city, setCity] = useState('');
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Search your city</h1>
+        </Col>
+      </Row>
+
+      <Row>
+
+        <Col xs={4} className="text-center">
+          <FormControl
+            placeholder="Enter city"
+
+            onChange={(event) => setCity(event.target.value)}
+
+            value={city}
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Button onClick={{onSearch}}>Check Weather</Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default CitySelector;
