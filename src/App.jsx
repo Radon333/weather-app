8f23ac30f8914be5cfa1c22b4b7e812523f2866c
Ranveer Shah <ranveer2001s@gmail.com>
2021-07-24 16:13:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home"
-import Current from "./Pages/Current/Current"
+import DisplayCurrent from "./Pages/Current/DisplayCurrent"
 function App() {  
   return (
     <Switch>
         <Route path="/current-weather">
-          <Current/>
+          <DisplayCurrent/>
         </Route>
         <Route path="/">
           <Home />
